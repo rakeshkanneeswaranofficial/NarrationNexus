1a97d9bb370bfa102a214d0659ca9c89e39816dd
Rakesh Kanneeswaran <rakeshkanneeswaran@Rakeshs-MacBook-Pro.local>
2024-02-18 23:21:37 +0530

    ui changed

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -3,7 +3,7 @@ export const Appbar = () => {
     <div className="shadow h-16 bg-black text-white flex justify-center border border-b-0 border-black items-center px-4">
       <div className="font-bold flex ">
         <h1 className="font-mono text-4xl mb-4 px-3">RadixAI</h1>
-        <h2 className="font-mono text-2xl  animate-pulse  underline mb-4">
+        <h2 className="font-mono text-2xl  animate-pulse   mb-4">
           Enhancing Medical Imaging with Artificial Intelligence and Computer Vision
         </h2>
       </div>
