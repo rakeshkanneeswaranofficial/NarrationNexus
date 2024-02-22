c205ad5f96aede39b7623e3c92631edd439e3b82
Rakesh kanneeswaran <rakeshkanneeswaran@Rakeshs-MacBook-Pro.local>
2024-02-22 14:44:42 +0530

    this

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const classifierURL = "https://detect.roboflow.com/main-x9qfk/1";
 const classifierAPI = "w2BkwXK1FTb2Z8DcWdkp";
 
 // URL for ngrok for local testing
-const ngrokURL = "https://3c9e-35-231-152-82.ngrok-free.app";
+const ngrokURL = "https://2fba-35-231-152-82.ngrok-free.app";
 
 const App = () => {
   return (
